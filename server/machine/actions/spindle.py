662c936b018a0e660ebef98a3c664b7cd38987e8
Vladislav Tsendrovskii <vlad@vltc.net.eu.org>
2019-02-14 15:22:48 +0300

    Using n700e control

--- server/machine/actions/spindle.py
+++ server/machine/actions/spindle.py
@@ -2,29 +2,29 @@ from . import action
 
 class SpindleOff(action.ToolAction):
 
-    def __init__(self, **kwargs):
+    def __init__(self, sender, **kwargs):
         action.ToolAction.__init__(self, **kwargs)
+        self.sender = sender
 
     def perform(self):
         print("Spindle off")
+        self.sender.stop()
         return True
 
 class SpindleOn(action.ToolAction):
 
-    def __init__(self, speed, cw, **kwargs):
+    def __init__(self, sender, speed, cw, **kwargs):
         action.ToolAction.__init__(self, **kwargs)
+        self.sender = sender
         self.speed = speed
         self.cw = cw
 
     def perform(self):
         print("Spindle on, speed = %lf" % self.speed)
+        self.sender.set_speed(self.speed)
+        if self.cw:
+            self.sender.start_forward()
+        else:
+            self.sender.start_reverse()
         return True
 
-class SpindleSetSpeed(action.ToolAction):
-    def __init__(self, speed, **kwargs):
-        action.ToolAction.__init__(self, **kwargs)
-        self.speed = speed
-
-    def perform(self):
-        print("Spindle speed = %lf" % self.speed)
-        return True
